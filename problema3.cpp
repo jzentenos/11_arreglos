a3d17d4489917f8c53ce1f91dfe25e1dd1264534
jzentenos <jzentenos@unjbg.edu.pe>
2024-07-06 01:18:04 -0500

    Ejercicio 3: Se agrego el punto 3, donde muestra los dias con temperatura maxima igual a una temperatura ingresada por el usuario, caso contrario muestra un mensaje.

--- problema3.cpp
+++ problema3.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cstdlib>
 using namespace std;
 int men_tem(float a[], int d);
 int main(){
@@ -11,13 +12,27 @@ int main(){
 		cout<<"Temperatura maxima: "; cin>>max[i];
 		med[i]= (max[i]+min[i]) / 2;
 	}
+	system("cls");
 	cout<<"\tTemperatura media de cada dia"<<endl;
 	for(int i=1; i<7; i++){
 		cout<<"Dia "<<i+1<<": "<<med[i]<<" grados"<<endl;
 	}
 	int dia= men_tem(min, 7);
-	cout<<"El dia "<<dia+1<<" es la menor temperatura de toda la semana con "<<min[dia]<<" grados"<<endl;
-		
+	cout<<"\tEl dia "<<dia+1<<" es la menor temperatura de toda la semana con "<<min[dia]<<" grados"<<endl;
+	system("cls");
+	float t; //t: Temperatura al azar ingresada por el usuario
+	cout<<"Ingrese una temperatura: "; cin>>t;
+	cout<<"\tTemperaturas maximas igual a "<<t<<" grados"<<endl;
+	int cc=0;
+	for(int i=0; i<7; i++){
+		if(t==max[i]){
+			cout<<"Dia: "<<i+1<<endl;
+			cc=1;
+		}
+	}
+	if(cc==0){
+		cout<<"No existe"<<endl;
+	}
 	return 0;
 }
 int men_tem(float a[], int d){
