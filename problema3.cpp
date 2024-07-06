980b90478952e55f3c5c5e0202a3276154a2981c
jzentenos <jzentenos@unjbg.edu.pe>
2024-07-06 00:56:46 -0500

    Ejercicio 3: Se agrego la funcion para hallar el dia con menor temperatura de la semana

--- problema3.cpp
+++ problema3.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+int men_tem(float a[], int d);
 int main(){
 	float min[7], max[7], med[7]; 
 	//min: temperatura minima del dia
@@ -10,7 +11,18 @@ int main(){
 		cout<<"Temperatura maxima: "; cin>>max[i];
 		med[i]= (max[i]+min[i]) / 2;
 	}
+	int dia= men_tem(min, 7);
+	cout<<"El dia "<<dia+1<<" es la menor temperatura de toda la semana con "<<min[dia]<<" grados"<<endl;
 		
 	return 0;
+}
+int men_tem(float a[], int d){
+	int min_dia=0;
+	for(int i=0; i<d; i++){
+		if( a[i] < a[min_dia]){
+			min_dia=i;
+		}
+	}
+	return min_dia;	
 }
 
