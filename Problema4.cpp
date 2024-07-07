62108668f4b05edf6478d28ecd860f75c1d93021
jzentenos <jzentenos@unjbg.edu.pe>
2024-07-07 01:07:23 -0500

    Ejercicio 4: Se creó un programa que rellena un arreglo de múltiplos de un número ingresados por el usuario

---
+++ Problema4.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+void rellena(int mul[], int a, int b);
+int main(){
+	int n;
+	//Lectura del tamaño del arreglo
+	cout<<"Ingrese el tamaño del arreglo: "; cin>>n;
+	int A[n], m;
+	//Lectura del multiplo a rellenar 
+	cout<<"Ingrese un numero: "; cin>>m;
+	rellena(A, n, m);
+	//Imprime el arreglo llenado de multiplos de m
+	cout<<"\tArreglo llenado con multiplos de "<<m<<endl;
+	for(int i=0; i<n; i++){
+		cout<<A[i]<<" ";
+	}
+	return 0;
+}
+//Funcion rellena: Rellena un arreglo de multiplos de un numero
+void rellena(int mul[],int a, int b){
+	for(int i=0; i<a; i++){
+		mul[i]=b*(i+1);
+	}
+}
